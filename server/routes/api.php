0123e8098582cc9f24c899fc97f774b37432d827
arkstudiodevelopers <nicksonogm@gmail.com>
2025-10-28 05:56:47 +0300

    working on securing the routes

--- server/routes/api.php
+++ server/routes/api.php
@@ -5,3 +5,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/ping', function () {
     return response()->json(['message' => 'API is working']);
 });
+
+Route::get('/users', [App\Http\Controllers\UserController::class, 'index'])->name('users');
